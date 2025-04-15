7a1f8754ac8d8bea6d50923989fc45093c0c83b7
bppham <phubao180803@gmail.com>
2025-04-15 20:54:55 +0700

    add: client-side access controll

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,20 +1,29 @@
 "use client";
+
 import Sidebar from "../components/Sidebar/Sidebar";
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import { usePathname } from "next/navigation";
+import RequireAuth from "../components/RequireAuth";
+
 export default function RootLayout({ children }) {
   const pathname = usePathname();
   const isAuthPage = pathname.startsWith("/auth");
 
+  const content = (
+    <>
+      {!isAuthPage && <Navbar />}
+      <div className="shipper-container">
+        {!isAuthPage && <Sidebar />}
+        <div className="shipper-container-content">{children}</div>
+      </div>
+    </>
+  );
+
   return (
     <html lang="en">
       <body>
-        {!isAuthPage && <Navbar />}
-        <div className="shipper-container">
-          {!isAuthPage && <Sidebar />}
-          <div className="shipper-container-content">{children}</div>
-        </div>
+        {isAuthPage ? content : <RequireAuth>{content}</RequireAuth>}
       </body>
     </html>
   );
